71912d1b42e2b5c2b665ede10821ab3ff05cb405
EdyBoc <ebocm1@miumg.edu.gt>
2024-04-08 21:35:25 -0600

    Correccion de vista dependencia

--- resources/views/dependencia/crear.blade.php
+++ resources/views/dependencia/crear.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
 <section class="section">
-        <div class="section-header">
-            <h3 class="page__heading">Crear Dependencia</h3>
-        </div>
-        <div class="section-body">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                        
-                        @if ($errors->any())                                                
-                            <div class="alert alert-dark alert-dismissible fade show" role="alert">
-                            <strong>¡Revise los campos!</strong>                        
-                                @foreach ($errors->all() as $error)                                    
-                                    <span class="badge badge-danger">{{ $error }}</span>
-                                @endforeach                        
+    <div class="section-header">
+        <h3 class="page__heading">Crear Dependencia</h3>
+    </div>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        @if ($errors->any())
+                        <div class="alert alert-dark alert-dismissible fade show" role="alert">
+                            <strong>¡Revise los campos!</strong>
+                            @foreach ($errors->all() as $error)
+                            <span class="badge badge-danger">{{ $error }}</span>
+                            @endforeach
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
-                            </div>
+                        </div>
                         @endif
 
 
@@ -28,18 +28,23 @@
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 col-md-12">
                                 <div class="form-group">
-                                    <label for="">Nombre de la Dependencia:</label>                                    
+                                    <label for="">Nombre de la Dependencia:</label>
                                     {!! Form::text('name', null, array('class' => 'form-control')) !!}
                                 </div>
                             </div>
-      
+
                         </div>
                         <button type="submit" class="btn btn-primary">Guardar</button>
+                        @can('crear-dependencia')
+                        <a class="btn btn-danger" href="{{ route('dependencia.index') }}">Cancelar</a>
+                        @endcan
                         {!! Form::close() !!}
-                        </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
