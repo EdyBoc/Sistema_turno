4609417239c4aa1d23acd813e7b7e0ae76a126b2
EdyBoc <ebocm1@miumg.edu.gt>
2024-05-19 23:45:09 -0600

    Correcciones

--- resources/views/sistemaTurnos/vacacion/index_vacaciones.blade.php
+++ resources/views/sistemaTurnos/vacacion/index_vacaciones.blade.php
@@ -61,45 +61,48 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h3 class="page__heading">Lista Vacaciones</h3>
+            <h4 class="page__heading">Lista Personas de Vacaciones</h4>
         </div>
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            <div class="row justify-content-center text-center">
-                                <div class="col-md-12">
-                                    <a class="btn btn-danger btn-lg" href="{{ route('index_listar') }}"><i
-                                            class="fas fa-reply"></i></i>Regrar</a>
-                                    <a class="btn btn-primary btn-lg" href="{{ route('crear_vacaciones') }}">
-                                        <i class="fas fa-street-view"></i> Nuevo
-                                    </a>
-                                    <a class="btn btn-primary btn-lg" id="btn_actualizar"><i class="fas fa-history"></i>
-                                        Actualizar</a>
-                                </div>
-                            </div>
+                            <div class="justify-content-center text-center">
+                                <a class="btn btn-danger btn-lg" href="{{ route('index_perfil') }}"><i
+                                        class="fas fa-reply"></i></i>Regrar</a>
 
+                                <a class="btn btn-primary btn-lg" id="btn_actualizar"><i class="fas fa-history"></i>
+                                    Actualizar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card ">
+                        <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-sm" id="tabla_vacacion" cellspacing="0" style="font-size: 80%;">
                                     <thead>
                                         <tr>
                                             <th class="col-1" style="text-align: center;"> No. </th>
+                                            <th class="col-2" style="text-align: center;"> Nombre</th>
                                             <th class="col-2" style="text-align: center;"> Fecha inicio</th>
                                             <th class="col-2" style="text-align: center;"> Fecha final</th>
-                                            <th style="text-align: center;"> Acciones</th>
+                                            <th class="col-2" style="text-align: center;"> Acciones</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($vacaciones as $vacacion)
                                             <tr>
                                                 <td style="text-align: center;">{{ $vacacion->id_vacacion }}</td>
+                                                <td style="text-align: center;">{{ $vacacion->id_persona }}</td>
                                                 <td style="text-align: center;">{{ $vacacion->fn_inicio }}</td>
                                                 <td style="text-align: center;">{{ $vacacion->fn_fin }}</td>
                                                 <td style="text-align: center;">
-                                                    <a class="btn-primary btn-sm" id="ver_detalle"><i
-                                                            class="far fa-eye"></i></a>
-                                                    <a class="btn-primary btn-sm" id="editar_vacacion"
+                                                    <a class="btn btn-primary btn-sm" id="editar_vacacion"
                                                         href="{{ route('crear_vacaciones', ['id_vacacion' => $vacacion['id_vacacion']]) }}"><i
                                                             class="far fa-edit"></i></a>
                                                 </td>
@@ -120,36 +123,6 @@
         $(document).ready(function() {
             $("#btn_actualizar").click(function() {
                 location.reload();
-            });
-        });
-
-        $(document).ready(function() {
-            $('#tabla_vacacion').DataTable({
-                rowReorder: true, // Permitir la reordenación de filas
-                lengthMenu: [
-                    [4, -1],
-                    [4, "All"]
-                ],
-                language: {
-                    "sProcessing": "Procesando...",
-                    "sLengthMenu": "Mostrar _MENU_ registros",
-                    "sZeroRecords": "No se encontraron resultados",
-                    "sEmptyTable": "Ningún dato disponible en la tabla",
-                    "sInfo": "Mostrando _START_ a _END_ de _TOTAL_ registros",
-                    "sInfoEmpty": "Mostrando 0 a 0 de 0 registros",
-                    "sInfoFiltered": "(filtrado de _MAX_ registros totales)",
-                    "sSearch": "Buscar:",
-                    "sPrevious": "Anterior",
-                    "sNext": "Siguiente",
-                    "sFirst": "Primero",
-                    "sLast": "Último",
-                    "oPaginate": {
-                        "sPrevious": "Anterior",
-                        "sNext": "Siguiente"
-                    },
-                    "sSortAsc": "Ordenar ascendente",
-                    "sSortDesc": "Ordenar descendente"
-                }
             });
         });
     </script>
