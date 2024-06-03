a3fde6c6e773f3e6ecd336af2c1891609c311606
EdyBoc <ebocm1@miumg.edu.gt>
2024-06-02 19:00:54 -0600

    test

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,25 +19,59 @@
 
                         <a class="btn btn-primary btn-lg" data-toggle="modal"
                             data-target="#modal_add_catalogo_dependencia"><i class="fas fa-street-view"></i>
-                            Nuevo</a>
+                            Nuevo registro</a>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="modal_add_catalogo_dependencia" tabindex="-1" role="dialog"
+        aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-primary text-white  text-center">
+                    <h5 class="modal-title  mx-auto">Nuevo Dependencia</h5>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="fr_catalogo_nombre">Nombre:</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="nombre_dependencia">
+                            <div class="input-group-append">
+                                <button class="btn btn-secondary" data-toggle="tooltip" title="Nombre">
+                                    <i class="fas fa-puzzle-piece"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="fr_catalogo_nombre">Descripcion:</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="descripcion_dependencia">
+                            <div class="input-group-append">
+                                <button class="btn btn-secondary" data-toggle="tooltip" title="descripcion_dependencia">
+                                    <i class="fas fa-puzzle-piece"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <a id="btnCerrar" onclick="$('#modal_add_catalogo_dependencia').modal('hide');"
+                        class="btn btn-danger btn-lg"><i class="fa fa-times"></i>&nbsp;Cancelar</a>
+                    &nbsp;
+                    <a href="#" id="btn_guardar_catalogo_dependencia" class="btn btn-success btn-lg"><i
+                            class="fas fa-save"></i>&nbsp;Guardar</a>
                 </div>
             </div>
         </div>
     </div>
 
 
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
     <script>
-        $(document).ready(function() {
-            $("#btn_actualizar").click(function() {
-                location.reload();
-            });
-        });
-
         $("#btn_guardar_catalogo_dependencia").click(function(e) {
             e.preventDefault();
             var nombre_dependencia = $("#nombre_dependencia").val();
