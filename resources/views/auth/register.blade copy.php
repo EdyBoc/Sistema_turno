7ee1314995285414eedbc9829977d4f362b67438
EdyBoc <ebocm1@miumg.edu.gt>
2024-06-02 18:57:50 -0600

    test

--- resources/views/auth/register.blade copy.php
+++ resources/views/auth/register.blade copy.php
@@ -1,165 +1,77 @@
 @extends('layouts.auth_app')
 @section('title')
-    Register
+Register
 @endsection
 @section('content')
-    <div class="card card-primary">
-        <div class="card-header">
-            <h4>Registrarme</h4>
-        </div>
-
-        <div class="card-body pt-1">
-            <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="first_name">Nombre de Usuario:</label><span class="text-danger">*</span>
-                            <input id="firstName" type="text"
-                                class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
-                                tabindex="1" placeholder="Enter Full Name" value="{{ old('name') }}" autofocus required>
-                            <div class="invalid-feedback">
-                                {{ $errors->first('name') }}
-                            </div>
-                        </div>
-
-
-                        <div class="form-group">
-                            <label for="cui">CUI:</label><span class="text-danger">*</span>
-                            <input id="cui" type="text"
-                                class="form-control{{ $errors->has('cui') ? ' is-invalid' : '' }}" name="cui"
-                                tabindex="1" placeholder="cui" value="{{ old('cui') }}" autofocus required>
-                            <div class="invalid-feedback">
-                                {{ $errors->first('cui') }}
-                            </div>
-                        </div>
-
-
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="email">Correo Electronico:</label><span class="text-danger">*</span>
-                            <input id="email" type="email"
-                                class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                placeholder="Enter Email address" name="email" tabindex="1" value="{{ old('email') }}"
-                                required autofocus>
-                            <div class="invalid-feedback">
-                                {{ $errors->first('email') }}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="password" class="control-label">Contraseña
-                                :</label><span class="text-danger">*</span>
-                            <input id="password" type="password"
-                                class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                placeholder="Set account password" name="password" tabindex="2" required>
-                            <div class="invalid-feedback">
-                                {{ $errors->first('password') }}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="password_confirmation" class="control-label">Confirmar Contraseña:</label><span
-                                class="text-danger">*</span>
-                            <input id="password_confirmation" type="password" placeholder="Confirm account password"
-                                class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
-                                name="password_confirmation" tabindex="2">
-                            <div class="invalid-feedback">
-                                {{ $errors->first('password_confirmation') }}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12 mt-4">
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                                Register
-                            </button>
-                            <a class="btn btn-primary btn-lg" data-toggle="modal"
-                                data-target="#modal_add_catalogo_dependencia"><i class="fas fa-street-view"></i>
-                                Nuevo</a>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-    <div class="mt-5 text-muted text-center">
-        Already have an account ? <a href="{{ route('login') }}">SignIn</a>
+<div class="card card-primary">
+    <div class="card-header">
+        <h4>Registrarme</h4>
     </div>
 
-    <div class="modal fade" id="modal_add_catalogo_dependencia" tabindex="-1" role="dialog"
-        aria-labelledby="myModalLabel">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header bg-primary text-white  text-center">
-                    <h5 class="modal-title  mx-auto">Nuevo Dependencia</h5>
-                </div>
-                <div class="modal-body">
+    <div class="card-body pt-1">
+        <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+                <div class="col-md-6">
                     <div class="form-group">
-                        <label for="fr_catalogo_nombre">Nombre:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="nombre_dependencia">
-                            <div class="input-group-append">
-                                <button class="btn btn-secondary" data-toggle="tooltip" title="Nombre">
-                                    <i class="fas fa-puzzle-piece"></i>
-                                </button>
-                            </div>
+                        <label for="first_name">Nombre de Usuario:</label><span class="text-danger">*</span>
+                        <input id="firstName" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" tabindex="1" placeholder="Enter Full Name" value="{{ old('name') }}" autofocus required>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('name') }}
                         </div>
                     </div>
+
+
                     <div class="form-group">
-                        <label for="fr_catalogo_nombre">Descripcion:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="descripcion_dependencia">
-                            <div class="input-group-append">
-                                <button class="btn btn-secondary" data-toggle="tooltip" title="descripcion_dependencia">
-                                    <i class="fas fa-puzzle-piece"></i>
-                                </button>
-                            </div>
+                        <label for="cui">CUI:</label><span class="text-danger">*</span>
+                        <input id="cui" type="text" class="form-control{{ $errors->has('cui') ? ' is-invalid' : '' }}" name="cui" tabindex="1" placeholder="cui" value="{{ old('cui') }}" autofocus required>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('cui') }}
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="email">Correo Electronico:</label><span class="text-danger">*</span>
+                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Enter Email address" name="email" tabindex="1" value="{{ old('email') }}" required autofocus>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('email') }}
                         </div>
                     </div>
                 </div>
-                <div class="modal-footer justify-content-center">
-                    <a id="btnCerrar" onclick="$('#modal_add_catalogo_dependencia').modal('hide');"
-                        class="btn btn-danger btn-lg"><i class="fa fa-times"></i>&nbsp;Cancelar</a>
-                    &nbsp;
-                    <a href="#" id="btn_guardar_catalogo_dependencia" class="btn btn-success btn-lg"><i
-                            class="fas fa-save"></i>&nbsp;Guardar</a>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="password" class="control-label">Contraseña
+                            :</label><span class="text-danger">*</span>
+                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Set account password" name="password" tabindex="2" required>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('password') }}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="password_confirmation" class="control-label">Confirmar Contraseña:</label><span class="text-danger">*</span>
+                        <input id="password_confirmation" type="password" placeholder="Confirm account password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" name="password_confirmation" tabindex="2">
+                        <div class="invalid-feedback">
+                            {{ $errors->first('password_confirmation') }}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 mt-4">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                            Register
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
-@endsection
-@section('scripts')
-    <script>
-        $(document).ready(function() {
-            $("#btn_actualizar").click(function() {
-                location.reload();
-            });
-        });
-
-        $("#btn_guardar_catalogo_dependencia").click(function(e) {
-            e.preventDefault();
-            var nombre_dependencia = $("#nombre_dependencia").val();
-            var descripcion_dependencia = $("#descripcion_dependencia").val();
-            $.ajax({
-                type: "POST",
-                url: "{{ route('guardar_dependencia') }}",
-                data: {
-                    nombre_dependencia: nombre_dependencia,
-                    descripcion_dependencia: descripcion_dependencia,
-                },
-                success: function(response) {
-                    if (!response.success) {
-                        toastr.error(response.message);
-                    } else {
-                        toastr.success(response.message);
-                        location.reload();
-                    }
-                }
-            });
-        });
-    </script>
+</div>
+<div class="mt-5 text-muted text-center">
+    Already have an account ? <a href="{{ route('login') }}">SignIn</a>
+</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,54 +1,36 @@
-@extends('layouts.auth_app')
-@section('title')
-    Register
-@endsection
-@section('content')
-    <a class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modal_add_catalogo_dependencia"><i
-            class="fas fa-street-view"></i>
-        Nuevo</a>
+<!DOCTYPE html>
+<html lang="en">
 
-    <div class="modal fade" id="modal_add_catalogo_dependencia" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header bg-primary text-white  text-center">
-                    <h5 class="modal-title  mx-auto">Nuevo Dependencia</h5>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="fr_catalogo_nombre">Nombre:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="nombre_dependencia">
-                            <div class="input-group-append">
-                                <button class="btn btn-secondary" data-toggle="tooltip" title="Nombre">
-                                    <i class="fas fa-puzzle-piece"></i>
-                                </button>
-                            </div>
-                        </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="justify-content-center text-center">
+
+                        <a class="btn btn-primary btn-lg" data-toggle="modal"
+                            data-target="#modal_add_catalogo_dependencia"><i class="fas fa-street-view"></i>
+                            Nuevo</a>
                     </div>
-                    <div class="form-group">
-                        <label for="fr_catalogo_nombre">Descripcion:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="descripcion_dependencia">
-                            <div class="input-group-append">
-                                <button class="btn btn-secondary" data-toggle="tooltip" title="descripcion_dependencia">
-                                    <i class="fas fa-puzzle-piece"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer justify-content-center">
-                    <a id="btnCerrar" onclick="$('#modal_add_catalogo_dependencia').modal('hide');"
-                        class="btn btn-danger btn-lg"><i class="fa fa-times"></i>&nbsp;Cancelar</a>
-                    &nbsp;
-                    <a href="#" id="btn_guardar_catalogo_dependencia" class="btn btn-success btn-lg"><i
-                            class="fas fa-save"></i>&nbsp;Guardar</a>
                 </div>
             </div>
         </div>
     </div>
-@endsection
-@section('scripts')
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     <script>
         $(document).ready(function() {
             $("#btn_actualizar").click(function() {
@@ -78,4 +60,8 @@
             });
         });
     </script>
-@endsection
+
+
+</body>
+
+</html>
