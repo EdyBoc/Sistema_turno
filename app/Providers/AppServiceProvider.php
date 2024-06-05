f6786047d2dd0be05dce6b15bf6ef929d1ee5f0d
chuites <carlosehs.9319@gmail.com>
2024-06-04 20:36:53 -0600

    Test para Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
