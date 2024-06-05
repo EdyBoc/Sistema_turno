d2e76ed4b26a70d09816285cac4112395c927a93
chuites <carlosehs.9319@gmail.com>
2024-06-04 20:43:59 -0600

    Correcciones de nombres

--- app/Models/SistemaTurnos/Vw_usuarios.php
+++ app/Models/SistemaTurnos/Vw_usuarios.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
-class VW_usuarios extends Model
+class Vw_usuarios extends Model
 {
     use HasFactory;
 
