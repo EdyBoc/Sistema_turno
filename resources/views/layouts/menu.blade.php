0a44e68eb84eec9ef82262aa0fcd341af8e0d6c0
enboc <edy.mingob@gmail.com>
2024-06-07 15:22:23 -0600

    Correccion

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,12 +3,6 @@
         <a class="nav-link" href="/home">
             <i class="fas fa-home"></i><span>Inicio</span>
         </a>
-        <a class="nav-link" href="/index_catalogo">
-            <i class="fas fa-book"></i><span></span>
-        </a>
-        <a class="nav-link" href="/index_asignacion">
-            <i class="fas fa-clipboard-list"></i><span></span>
-        </a>
     </li>
 @else
     <a class="nav-link" href="/home">
