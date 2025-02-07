fa19175fef06310e8fe0d0dfc81bd1c9636bb63b
enboc <edy.mingob@gmail.com>
2025-02-07 16:00:00 -0600

    Cambios de referencia rutas librerias

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,16 +9,15 @@
         <li class="dropdown">
             <a href="#" data-toggle="dropdown"
                class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                <img alt="image" src="{{ secure_asset('img/logo.png') }}"
+                <img alt="image" src="{{ asset('img/logo.png') }}"
                      class="rounded-circle mr-1 thumbnail-rounded user-thumbnail ">
                 <div class="d-sm-none d-lg-inline-block">
-                    ¡Bienvenido! {{\Illuminate\Support\Facades\Auth::user()->first_name}}</div>
-
+                    ¡Hola! {{\Illuminate\Support\Facades\Auth::user()->first_name}}</div>
             </a>
 
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-title">
-                    {{\Illuminate\Support\Facades\Auth::user()->name}}</div>
+                    Bienvenido, {{\Illuminate\Support\Facades\Auth::user()->name}}</div>
                 <a class="dropdown-item has-icon edit-profile" href="#" data-id="{{ \Auth::id() }}">
                     <i class="fa fa-user"></i>Editar Perfil de Usuario</a>
                 <a class="dropdown-item has-icon" data-toggle="modal" data-target="#changePasswordModal" href="#" data-id="{{ \Auth::id() }}"><i
